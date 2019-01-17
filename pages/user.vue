a3b65d8579450dbcfc46d0bd683372e9921be330
koci <koci@zldeMacBook-Air-6.local>
2019-01-17 19:20:45 +0800

    feat: add axions 拦截配置

--- pages/user.vue
+++ pages/user.vue
@@ -1,3 +1,17 @@
 <template>
-  <div>user list page</div>
+  <div>user list page
+    <el-button @click="goBack">回退</el-button>
+  </div>
 </template>
+
+
+<script>
+export default {
+  methods: {
+    goBack() {
+      console.log(this.$router.back);
+      this.$router.back();
+    }
+  }
+};
+</script>
--- plugins/axios.js
+++ plugins/axios.js
@@ -1,11 +1,26 @@
-export default function({ $axios, redirect, ...other }) {
+export default function({ $axios, redirect, app, ...other }) {
   $axios.onRequest(config => {
-    console.log('Making request to ' + config.url);
+    config.headers['Content-Type'] = 'application/json';
+    let token = 'get token';
+    if (token) {
+      config.headers['x-auth-token'] = token;
+    }
+    return config;
+  });
+
+  $axios.onResponse(response => {
+    const { data } = response;
+    console.log({ ...other });
+    // 拦截状态做重定项
+    if (data.status === '401.1') {
+      app.router.push('/user');
+      return;
+    }
   });
 
   $axios.onError(error => {
+    // 网络异常等其他情况
     const code = parseInt(error.response && error.response.status);
-    // console.log('onError axios');
     if (code === 400) {
       redirect('/400');
     }
